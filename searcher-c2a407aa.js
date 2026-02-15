731342b38730fc5d8d5d3e4c98b1f0e30a31aca0
Li-Hongmin <Li-Hongmin@users.noreply.github.com>
2026-02-15 05:51:47 +0000

    deploy: 6d47b711f9c9421ecdf249ae2383824b1d2c9c1a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b56c98a7.js',
+                path_to_root + 'searchindex-9ec82472.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
